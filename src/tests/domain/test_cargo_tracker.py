8bc4f51e615fbbd4b0607d64fa43e2d9723fc444
Piotr Kopalko <copalco@gmail.com>
2020-04-05 12:58:55 +0200

    Add cargo tracker

---
+++ src/tests/domain/test_cargo_tracker.py
@@ -0,0 +1,9 @@
+import unittest
+
+from transport_tycoon.tracker import CargoTracker
+
+
+class TestCargoTracker(unittest.TestCase):
+
+    def test_knows_when_cargo_was_loaded_on_vehicle(self) -> None:
+        CargoTracker()
---
+++ src/transport_tycoon/tracker.py
@@ -0,0 +1,2 @@
+class CargoTracker:
+    pass
