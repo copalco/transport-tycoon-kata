8f47e6d3512982d46cf631b1e5a21f1a6b781cb7
Piotr Kopalko <copalco@gmail.com>
2020-04-05 23:02:08 +0200

    Add loaded method to cargo tracker

--- src/tests/domain/test_cargo_tracker.py
+++ src/tests/domain/test_cargo_tracker.py
@@ -1,6 +1,7 @@
 import unittest
 
 from transport_tycoon.tracker import CargoTracker
+from transport_tycoon.domain.cargo_loaded import CargoLoaded
 
 
 class TestCargoTracker(unittest.TestCase):
@@ -8,3 +9,4 @@ class TestCargoTracker(unittest.TestCase):
     def test_knows_when_cargo_was_loaded_on_vehicle(self) -> None:
         tracker = CargoTracker()
         tracker.loaded()
+        assert tracker.events == [CargoLoaded()]
