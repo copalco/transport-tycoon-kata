6594f2da5b715b1ef49037c23e682dc38b954a65
Piotr Kopalko <copalco@gmail.com>
2020-04-05 12:57:52 +0200

    Add failing acceptance test

--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -8,3 +8,8 @@ class TestTransportTycoonApp(unittest.TestCase):
     def test_delivering_no_cargo_takes_no_time(self) -> None:
         app = TransportTycoonApp()
         self.assertEqual(app.deliver(cargo=[]), 0)
+
+    @unittest.expectedFailure
+    def test_calculates_delivery_of_cargo_to_warehouse_b(self) -> None:
+        app = TransportTycoonApp()
+        self.assertEqual(app.deliver(cargo=['B']), 5)
