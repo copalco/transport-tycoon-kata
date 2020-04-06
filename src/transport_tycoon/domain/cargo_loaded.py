58d4b671039c98e7b83687e2fea7f21e1182d2f7
Piotr Kopalko <copalco@gmail.com>
2020-04-06 18:25:59 +0200

    Loaded method creates new event

--- src/transport_tycoon/domain/cargo_loaded.py
+++ src/transport_tycoon/domain/cargo_loaded.py
@@ -1,3 +1,7 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
 class CargoLoaded:
 
-
+    pass
--- src/transport_tycoon/tracker.py
+++ src/transport_tycoon/tracker.py
@@ -1,4 +1,12 @@
+import typing
+
+from transport_tycoon.domain.cargo_loaded import CargoLoaded
+
+
 class CargoTracker:
 
+    def __init__(self) -> None:
+        self.events: typing.List[CargoLoaded] = []
+
     def loaded(self) -> None:
-        pass
+        self.events.append(CargoLoaded())
