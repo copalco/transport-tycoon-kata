6cdeb69ac381d3ffb64e042f2f67a5be98e481f4
Piotr Kopalko <copalco@gmail.com>
2020-04-06 18:52:57 +0200

    Record delivery events

--- src/tests/domain/test_cargo_tracker.py
+++ src/tests/domain/test_cargo_tracker.py
@@ -1,5 +1,6 @@
 import unittest
 
+from transport_tycoon.domain.cargo_delivered import CargoDelivered
 from transport_tycoon.tracker import CargoTracker
 from transport_tycoon.domain.cargo_loaded import CargoLoaded
 
@@ -10,3 +11,8 @@ class TestCargoTracker(unittest.TestCase):
         tracker = CargoTracker()
         tracker.cargo_has_been_loaded()
         assert tracker.events == [CargoLoaded()]
+
+    def test_knows_when_cargo_has_been_delivered(self) -> None:
+        tracker = CargoTracker()
+        tracker.cargo_has_been_delivered()
+        self.assertEqual(tracker.events, [CargoDelivered()])
--- src/transport_tycoon/tracker.py
+++ src/transport_tycoon/tracker.py
@@ -1,12 +1,16 @@
 import typing
 
+from transport_tycoon.domain.cargo_delivered import CargoDelivered
 from transport_tycoon.domain.cargo_loaded import CargoLoaded
 
 
 class CargoTracker:
 
     def __init__(self) -> None:
-        self.events: typing.List[CargoLoaded] = []
+        self.events: typing.List[typing.Union[CargoLoaded, CargoDelivered]] = []
 
     def cargo_has_been_loaded(self) -> None:
         self.events.append(CargoLoaded())
+
+    def cargo_has_been_delivered(self) -> None:
+        self.events.append(CargoDelivered())
