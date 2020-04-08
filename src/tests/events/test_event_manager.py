1d516ea503240fce736ff827d65c3babe18e1e7e
Piotr Kopalko <copalco@gmail.com>
2020-04-08 18:37:12 +0200

    Add routing for events

--- src/tests/events/test_event_manager.py
+++ src/tests/events/test_event_manager.py
@@ -2,6 +2,7 @@ import unittest
 from typing import Optional
 
 from transport_tycoon.domain.cargo_delivered import CargoDelivered
+from transport_tycoon.domain.cargo_loaded import CargoLoaded
 from transport_tycoon.domain.event import DomainEvent
 from transport_tycoon.events.event_manager import EventManager
 
@@ -29,6 +30,16 @@ class TestEventManager(unittest.TestCase):
         manager.record(CargoDelivered())
         self.assertEqual(first_subscriber.received, CargoDelivered())
         self.assertEqual(second_subscriber.received, CargoDelivered())
+
+    def test_subscriber_wont_get_event_it_does_not_listen_for(self) -> None:
+        first_subscriber = FakeDeliveredSubcriber()
+        second_subscriber = FakeDeliveredSubcriber()
+        manager = EventManager()
+        manager.subscribe_for(CargoDelivered, first_subscriber.handle_cargo_delivered)
+        manager.subscribe_for(CargoLoaded, second_subscriber.handle_cargo_delivered)
+        manager.record(CargoDelivered())
+        self.assertEqual(first_subscriber.received, CargoDelivered())
+        self.assertIsNone(second_subscriber.received)
 
 
 class FakeDeliveredSubcriber:
